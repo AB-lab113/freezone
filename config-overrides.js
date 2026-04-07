d0c65ce7fa456be31ab860fcd69e54539b6c33cc
AB-lab113 <a69c@protonmail.com>
2026-04-07 18:05:25 +0200

    revert: react-scripts pur, supprime react-app-rewired + config-overrides

--- config-overrides.js
+++
@@ -1,5 +0,0 @@
-module.exports = function override(config) {
-  // Désactiver le scope hoisting qui cause les TDZ (variables 'Ze' before init)
-  config.optimization.concatenateModules = false
-  return config
-}
