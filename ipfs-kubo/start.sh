133602137bf758088d3e51c2bac969723f6e4f12
AB-lab113 <a69c@protonmail.com>
2026-05-11 03:46:57 +0200

    feat: add blabla.privacy redirect CID

--- ipfs-kubo/start.sh
+++ ipfs-kubo/start.sh
@@ -3,12 +3,23 @@ ipfs init --profile=server
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+
 mkdir -p /tmp/site
 echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=https://zonefreeweb.app.runonflux.io"><title>ZoneFree</title></head><body><a href="https://zonefreeweb.app.runonflux.io">Redirecting...</a></body></html>' > /tmp/site/index.html
+
+mkdir -p /tmp/blabla
+echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=https://blablaprivacy.app.runonflux.io"><title>Blabla.Privacy</title></head><body><a href="https://blablaprivacy.app.runonflux.io">Redirecting...</a></body></html>' > /tmp/blabla/index.html
+
 ipfs daemon &
 DAEMON_PID=$!
 sleep 20
+
 CID=$(ipfs add -r -Q /tmp/site)
-echo "REDIRECT_CID=$CID"
+echo "REDIRECT_CID_ZONEFREE=$CID"
 ipfs pin add $CID
+
+CID_BLABLA=$(ipfs add -r -Q /tmp/blabla)
+echo "REDIRECT_CID_BLABLA=$CID_BLABLA"
+ipfs pin add $CID_BLABLA
+
 wait $DAEMON_PID
